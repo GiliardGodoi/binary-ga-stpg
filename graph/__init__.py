04f896bf2ea9f194cbaf1c26ffd041a8dbf6ccd5
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2019-11-01 00:19:38 -0300

    Lendo o arquivo com a instância do problema

---
+++ graph/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+    Arquivo de inicialização do módulo.
+
+    É necessário expor os módulos que se quer exportar
+'''
+from graph.reader import Reader
+from graph.reader import SteinerTreeProblem
---
+++ graph/reader.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+'''
+A Função principal dessa classe é fazer o parse da instância do problema
+e criar uma representação em memória.
+'''
+
+import re
+from collections import defaultdict
+
+class SteinerTreeProblem(object):
+
+    def __init__(self):
+        self.nro_nodes = 0
+        self.nro_edges = 0
+        self.nro_terminals = 0
+        self.graph = defaultdict(dict)
+        self.terminals = list()
+
+        self.name = None
+        self.remark = None
+        self.creator = None
+        self.file_name = None
+
+
+class Reader(object):
+
+    def __init__(self):
+        self.STP = SteinerTreeProblem()
+
+    def parser(self, fileName):
+        self.STP.file_name = fileName
+
+        with open(fileName, 'r') as file :
+            for line in file :
+                if "SECTION Comment" in line :
+                    self.parser_section_comment(file)
+
+                elif "SECTION Graph" in line :
+                    self.parser_section_graph(file)
+
+                elif "SECTION Terminals" in line :
+                    self.parser_section_terminals(file)
+
+        return self.STP
+
+    def parser_section_comment(self,file):
+        for line in file:
+            _list = re.findall(r'"(.*?)"',line)
+            if "Name" in line :
+                _name = _list[0] if len(_list) else "Name unviable"
+                self.STP.name = _name
+
+            elif "Creator" in line :
+                _creator = _list[0] if len(_list) else "Creator unviable"
+                self.STP.creator = _creator
+
+            elif "Remark" in line : 
+                remark = _list[0] if len(_list) else "Creator unviable"
+                self.STP.remark = remark
+
+            elif "END" in line:
+                break
+
+    def parser_section_graph(self, file):
+        for line in file:
+            if line.startswith("E ") :
+                entries = re.findall(r'(\d+)', line)
+                vetor = [ e for e in entries if e.isdecimal() ]
+
+                assert len(vetor) == 3, "Lendo uma linha com mais de três valores"
+                v, w, peso = vetor
+
+                v = int(v)
+                w = int(w)
+                peso = int(peso)
+
+                self.STP.graph[v][w] = peso
+                self.STP.graph[w][v] = peso
+
+            elif line.startswith("Nodes"):
+                nodes = re.findall(r'Nodes (\d+)$', line)
+                self.STP.nro_nodes = int(nodes[0]) if len(nodes) else -1
+
+            elif line.startswith("Edges"):
+                edges = re.findall(r'Edges (\d+)$', line)
+                self.STP.nro_edges = int(edges[0]) if len(edges) else -1
+
+            elif "END" in line :
+                break
+
+    def parser_section_terminals(self,file):
+
+        for line in file:
+            if line.startswith("T "):
+                _string = re.findall(r"(\d+)$", line)
+                v_terminal = _string[0] if len(_string) == 1 else -1
+                self.STP.terminals.append(v_terminal)
+
+            elif line.startswith("Terminals"):
+                terminal = re.findall(r'Terminals (\d+)$', line)
+                self.STP.nro_terminals = int(terminal[0]) if len(terminal) else -1
+
+            elif "END" in line:
+                break
