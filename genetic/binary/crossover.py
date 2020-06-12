af3eec2fe8c683139d19893ff614f47118e2f2d0
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2020-06-12 17:35:53 -0300

    a more efficient n point crossover

--- genetic/binary/crossover.py
+++ genetic/binary/crossover.py
@@ -1,5 +1,7 @@
 
 from random import choice, random, sample
+from bisect import bisect
+from itertools import cycle
 
 def crossover_2points(parent_a, parent_b):
     length_a, length_b = len(parent_a), len(parent_b)
@@ -19,9 +21,45 @@ def crossover_1point(parent_a, parent_b):
 def crossover_Npoints(parent_a, parent_b, n=2):
     length_a, length_b = len(parent_a), len(parent_b)
     assert length_a == length_b, "chromosomes doesn't have the same length"
-    points = sample(range(0,length_a), k=n)
+    assert n < (length_a - 1) , f"It is allowed only {(length_a - 1)} cuts. It was given {n}"
 
-    raise NotImplementedError
+    choose_parents = cycle("AB")
+    parents = [ next(choose_parents) for _ in range(n+1) ]
+    breakpoints = sample(range(0,length_a+1), k=n)
+    breakpoints.sort()
+
+    def choose(idx):
+        return parents[bisect(breakpoints, idx)]
+
+    newchromosome = [ genes[0] if choose(idx) == "A" else genes[1] for idx, genes in enumerate(zip(parent_a, parent_b))]
+
+    if isinstance(parent_a, str):
+        newchromosome = ''.join(newchromosome)
+
+    return newchromosome
+
+def crossover_nbreakpoints(parent_a, parent_b, n=2):
+
+    raise RuntimeWarning("It doesnt works for n > 2")
+    length_a, length_b = len(parent_a), len(parent_b)
+    assert length_a == length_b, "chromosomes doesn't have the same length"
+    assert n <= (length_a - 1) , f"It is allowed only {(length_a - 1)} cuts. It was given {n}"
+
+    def chunck(parent,start, end):
+        return parent[start:end]
+
+    points = [0] + sample(range(0, length_a+1), k=n) + [length_a]
+    flag = True
+    newchromosome = list()
+    for start, end in zip(points, points[1:]):
+        if flag:
+            newchromosome.append(chunck(parent_a, start, end))
+        else:
+            newchromosome.append(chunck(parent_b, start, end))
+        flag = not flag
+
+    return ''.join(newchromosome)
+
 
 def crossover_uniform(chromosome_a, chromosome_b, pbcrossover=0.5, **kwargs):
 
