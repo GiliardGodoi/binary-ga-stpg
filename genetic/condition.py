641645c9828683791089cd7a9f22fde349ee2d21
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2020-06-13 12:31:14 -0300

    parametrized conditions

--- genetic/condition.py
+++ genetic/condition.py
@@ -3,12 +3,38 @@ class condition:
 
     conditions = set()
 
-    def __init__(self, func):
-        self.conditions.add(func)
+    def __init__(self, func, msg="Stop condition reached", active=True):
+        if active:
+            self.conditions.add(func)
         self.func = func
+        self.msg = msg
+        self._active = active
 
     def __call__(self,*args, **kwargs):
         return self.func(*args, **kwargs)
+
+    @property
+    def active(self):
+        return self._active
+
+    @active.setter
+    def active(self, value):
+        if value :
+            self._active = True
+            self.conditions.add(self.func)
+        else :
+            self._active = False
+            self.conditions.remove(self.func)
+
+    @classmethod
+    def params(cls, msg="Stop condition reached", active=True):
+
+        def decorator(func):
+            print(func.__name__)
+            return cls(func, msg=msg, active=active)
+
+        return decorator
+
 
     @classmethod
     def check(cls, population):
--- test_simulation.py
+++ test_simulation.py
@@ -1,24 +1,60 @@
 
-from genetic.simulator import simulation
+from genetic.binary import random_binary
+from genetic.population import BasePopulation as Population
+from genetic.binary.crossover import crossover_2points, crossover_uniform
+from genetic.binary.mutate import flip_nbit
+from genetic.binary.selection import roullete
 from genetic.condition import condition
+from genetic.simulator import simulation
+from util import instance_problem
+from tools import evaluate_binary
+import time
 
-from random import normalvariate
+MAX_GENERATION = 10
 
-MAX_GENERATION = 100
+POPULATION_SIZE = 100
+
+STPG = instance_problem("datasets", "ORLibrary","steinb15.txt")
+
+chromosome_length = STPG.nro_nodes - STPG.nro_terminals
 
 @condition
-def stopby_maxlimit(number):
-    return number < MAX_GENERATION
+def stopby_maxgeneration(population : "Population"):
+    return population.generation < MAX_GENERATION
 
-@simulation(name="normal distribution", mean=50, std=15)
-def test_simulation(max_trial, mean=0, std=5, **kwargs):
+@condition.params(active=False)
+def stopby_maxfitness(population):
+    return False
 
-    samples = list()
+def eval(STPG):
+    def wrapps(chromosome):
+        return evaluate_binary(chromosome, STPG.graph, STPG.terminals, STPG.nro_nodes, lambda k : (k-1) * 100 )
 
-    count = 0
-    while condition.check(count):
-        sample = normalvariate(mean, std)
-        samples.append(sample)
-        count += 1
+    return wrapps
 
-    return samples
+def set_loggers(dst):
+    pass
+
+
+def test_simulation():
+
+    population = Population(chromosomes=[random_binary(chromosome_length) for _ in range(POPULATION_SIZE)],
+                            evaluation_func=eval(STPG)).evaluate().normalize()
+
+    start = time.time()
+    while condition.check(population):
+        print(condition.conditions)
+        print("running")
+        population.select(selector_func=roullete)
+        population.recombine(mate_func=crossover_2points)
+        population.mutation(mutate_func=flip_nbit)
+        population.evaluate()
+        population.normalize()
+        population.generation += 1
+
+    runtime = time.time() - start
+
+    print(runtime)
+
+# test_simulation()
+
